6313b079f4f6a0929be912d590c79d2e7511e7d6
apcmmorenze <mmorenze@student.apc.edu.ph>
2025-01-31 04:49:23 +0800

    Updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -9,11 +9,10 @@
 
 <style>
   #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
 </style>
