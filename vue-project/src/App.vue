f4d6f3c0cbd2a0ffd29e6e6878cf796afc13bdf7
apcmmorenze <mmorenze@student.apc.edu.ph>
2025-02-04 19:39:30 +0800

    Updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,7 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-
-onMounted(() => {
-  getInstruments()
-})
-</script>
-
+<script></script>
+ 
 <template>
-  <h1>Instruments</h1>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
-  <h2>Food</h2> 
-  <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
+  <h1>Hello World</h1>
 </template>
-
+ 
 <style></style>
--- vue-project/src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://ivjcbellwhtotulkthov.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2amNiZWxsd2h0b3R1bGt0aG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1ODMzMTQsImV4cCI6MjA1NDE1OTMxNH0.WveDkCUj_1uX5rgrjdyeO8ifmyj5TGjh_5w5QV73Iwo')
