cdf1de511e1f873dd990c3af9d2d6b1e6b512c05
apcmmorenze <mmorenze@student.apc.edu.ph>
2025-02-10 20:45:04 +0800

    Updated

---
+++ vue-project/src/components/Comment.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const comment = ref([])
+
+async function getComment() {
+  const { data } = await supabase.from('comment').select()
+  comment.value = data
+}
+
+onMounted(() => {
+  getComment()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="comment in comment" :key="comment.id">{{ comment.name }}</li>
+  </ul>
+</template>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import Instruments from './components/Instruments.vue'
+import Comment from './components/Comment.vue'
+import CommentForm from './components/CommentForm.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
  
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
+app.component('instruments', Instruments)
+app.component('comment', Comment)
+app.component('comment-form', CommentForm)
 app.component('personal-profile', PersonalProfile)
 app.mount('#app')
