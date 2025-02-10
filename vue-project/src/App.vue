af9cde6957ee0bb98c4da581eae8647c212e25a9
apcmmorenze <mmorenze@student.apc.edu.ph>
2025-02-10 21:09:39 +0800

    Updated code

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>Food</h1>
   <personal-profile/> <br>
+  <h1>Food</h1>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
   <food-item2/> <br>
+  <h1>Intruments</h1>
   <instruments/> <br>
   <comment-form/> <br>
   <comment/> <br>
